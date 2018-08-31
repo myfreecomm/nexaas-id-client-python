8195585c84148eef4e4ec229a697f68a5b97798a
Rodrigo Cacilhas <rodrigo.cacilhas@nexaas.com>
2018-08-31 18:23:03 -0300

    update missing data

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
+from pathlib import Path
 from setuptools import setup, find_packages
+
+with (Path(__file__).parent / 'README.md').open() as fp:
+    long_description = fp.read()
 
 
 setup(
     name = 'nexaas_id_client',
-    version = '1.0',
+    version = '1.0.1',
     author = 'Rodrigo Cacilhas',
     author_email = 'rodrigo.cacilhas@nexaas.com',
-    description = '',
+    description = 'Nexaas ID Python client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license = 'Proprietary License',
     keywords = 'nexaas-id oauth',
-    url = '',
+    url = 'https://github.com/myfreecomm/nexaas-id-client-python',
     packages = find_packages(exclude=('tests', 'tests.*')),
-    long_description = '',
     test_suite = 'tests',
     install_requires = [
         'python-dateutil>=2.7.0',
